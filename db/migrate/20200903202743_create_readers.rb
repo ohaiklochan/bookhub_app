2aa695e2495cb3e95b09a0f571f898c478084ed3
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-09-03 16:12:05 -0500

    first commit

---
+++ db/migrate/20200903202743_create_readers.rb
@@ -0,0 +1,9 @@
+class CreateReaders < ActiveRecord::Migration[6.0]
+  def change
+    create_table :readers do |t|
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
---
+++ db/migrate/20200903202855_create_books.rb
@@ -0,0 +1,12 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title
+      t.string :genre
+      t.text :description
+      t.integer :author_id
+
+      t.timestamps
+    end
+  end
+end
