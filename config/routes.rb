4a2dc001c72c51eb70af860abb0d20aa75fe9bce
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-09-10 14:57:47 -0500

    changed style sheets

--- config/routes.rb
+++ config/routes.rb
@@ -1,35 +1,30 @@
 Rails.application.routes.draw do
-  get 'reviews/index'
-  get 'reviews/new'
-  get 'reviews/create'
-  get 'reviews/show'
-  get 'reviews/edit'
-  get 'reviews/update'
-  get 'reviews/destroy'
-  get 'sessions/login'
-  get 'sessions/create'
-  get 'sessions/destroy'
-  get 'sessions/omniauth'
-  get 'readers/new'
-  get 'readers/create'
-  get 'readers/edit'
-  get 'readers/update'
-  get 'readers/destroy'
-  get 'readers/index'
-  get 'readers/show'
-  get 'books/new'
-  get 'books/create'
-  get 'books/edit'
-  get 'books/update'
-  get 'books/destroy'
-  get 'books/index'
-  get 'books/show'
-  get 'authors/new'
-  get 'authors/create'
-  get 'authors/edit'
-  get 'authors/update'
-  get 'authors/destroy'
-  get 'authors/index'
-  get 'authors/show'
+  root "sessions#home"
+
+  # signup
+  get "/signup" => "users#new"
+  # if a user signs up incorrectly the url re-renders as /signup
+  post "/signup" => "users#create"
+
+  # login
+  get "/login" => "sessions#new"
+  post "/login" => "sessions#create"
+
+  # logout
+  delete "/logout" => "sessions#destroy"
+
+  # omniauth callback routes
+  get "/auth/:provider/callback" => "sessions#omnilogin"
+
+  resources :reviews, :books
+  resources :authors, only: [:destroy]
+  resources :authors, only: [:index]
+  resources :readers, only: [:show]
+  resources :books, :users, only: [:show] do
+    resources :reviews, shallow: true
+  end
+  resources :authors, only: [:show] do
+    resources :bookss, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
