841fc4f1e6eb23adf6db9e0cc55e99d8d2b7bf0d
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-09-10 01:28:36 -0500

    model edits

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,16 @@
 class Author < ApplicationRecord
-    has_many :books
-    has_many :reviews
+    has_many :books, through: :readers, :dependent => :destroy
     belongs_to :readers
+    validates_presence_of :first_name, :last_name
+  
+    scope :find_by_first_name, -> (first_name) { where("first_name LIKE ?", first_name) }
+    scope :find_by_last_name, -> (last_name) { where("last_name LIKE ?", last_name) }
+  
+    def full_name
+        if self.last_name
+            self.first_name + " " + self.last_name
+        else
+            self.first_name
+        end
+    end
 end
--- app/models/reader.rb
+++ app/models/reader.rb
@@ -1,10 +1,20 @@
 class Reader < ApplicationRecord
-    has_many :books
-    has_many :authors, through: :books
-
+    has_many :authors
+    has_many :reviews
+    has_many :books, through: :authors
     has_secure_password
-
-    # validates :username, presence: true
-    # validates :username, uniqueness: {message: "Username is already in use."}
-
+    validates_presence_of :username, :email
+    validates :username, :email, :uniqueness => true
+  
+    def self.from_omniauth(auth)
+        @reader = Reader.find_or_create_by(:uid => auth[:uid]) do |reader|
+            if !Reader.find_by(:username => auth[:info][:first_name])
+                reader.username = auth[:info][:first_name]
+            else
+                reader.username = auth[:info][:first_name] + auth[:uid]
+            end
+            reader.email = auth[:info][:email]
+            reader.password = SecureRandom.hex
+        end
+    end
 end
