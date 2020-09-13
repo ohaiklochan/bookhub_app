f19c3e82dc4965231d2f0a2016144f2b5a513670
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-09-13 17:36:58 -0500

    edited users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,18 +4,18 @@ class UsersController < ApplicationController
 
   
   def new
-      @user = User.new
+    @user = User.new
   end
 
   def create 
-        @user = User.new(user_params)
-        if @user.save
-          session[:user_id] = @user.id 
-          redirect_to reviews_path(@user)
-        else 
-          redirect_to new_user_path
-          flash[:error] = "Username is not available, please use a unique username."
-        end
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id 
+      redirect_to reviews_path(@user)
+    else 
+      redirect_to new_user_path
+      flash[:error] = "Username is not available, please use a unique username."
+    end
   end
   
   def show
