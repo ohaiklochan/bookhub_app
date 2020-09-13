c5f6c7665e9298306befadd8806d5b830846e44c
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-09-12 22:37:13 -0500

    added a sorting table

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+    def sortable(column, title = nil)
+        title ||= column.titleize
+        direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+        link_to title, {:sort => column, :direction => direction}
+      end
+    
+      def sort_image(column)
+        if sort_direction == "asc" && column == sort_column
+          " ▲"
+        elsif sort_direction == "desc" && column == sort_column
+          " ▼"
+        end
+      end
 end
