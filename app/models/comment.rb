1954e686ac716d62456b50a10068e7c7a22476fd
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-09-13 17:23:06 -0500

    added model 'comment'

---
+++ app/models/comment.rb
@@ -0,0 +1,20 @@
+class Comment < ApplicationRecord
+    belongs_to :user 
+    belongs_to :review
+    scope :most_recent, -> {Comment.order("created_at DESC")}
+
+
+    def self.deleted_user
+        Comment.all.each do |comment|
+            if comment.user == nil
+                comment.destroy 
+            end
+        end 
+    end 
+
+
+    def normalize_date
+        self.created_at = created_at.strftime("%B %-d, %Y %-l:%M %p")
+    end 
+
+end
