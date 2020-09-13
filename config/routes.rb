626709f82ac9d2e962ff60c54ba8b8f0a37b10cf
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-09-13 17:42:39 -0500

    edited routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,30 +1,20 @@
 Rails.application.routes.draw do
-  root "sessions#home"
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
+  get '/', to: 'application#welcome' 
+  get '/login', to: 'sessions#login'
+  post '/login', to: 'sessions#create'
+  get '/auth/google_oauth2/callback', to: 'sessions#omniauth'
+  
 
-  # signup
-  get "/signup" => "users#new"
-  # if a user signs up incorrectly the url re-renders as /signup
-  post "/signup" => "users#create"
-
-  # login
-  get "/login" => "sessions#new"
-  post "/login" => "sessions#create"
-
-  # logout
-  delete "/logout" => "sessions#destroy"
-
-  # omniauth callback routes
-  get "/auth/:provider/callback" => "sessions#omnilogin"
-
-  resources :reviews, :books
-  resources :authors, only: [:destroy]
-  resources :authors, only: [:index]
-  resources :readers, only: [:show]
-  resources :books, :users, only: [:show] do
-    resources :reviews, shallow: true
+  resources :users, only: [:new, :show, :create, :edit, :update, :destroy]
+  resources :journeys do 
+  resources :comments, only: [:new, :create, :show, :index]
   end
-  resources :authors, only: [:show] do
-    resources :bookss, only: [:index]
-  end
+
+  delete '/logout', to: 'sessions#destroy'
+  root 'application#welcome' 
+end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
