16487ee27e0669d255b5bc863a42726c677b1ad7
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-09-13 17:21:45 -0500

    edited reviews path

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,13 +1,22 @@
 class Review < ApplicationRecord
-    belongs_to :reader
-    belongs_to :book
-    validate :one_review_per_reader_per_book
-    validates_presence_of :rating, :title, :content
-
-    def one_review_per_reader_per_book
-        book_reviews = reader.reviews.select { |review| review.book_id == self.book_id }
-        if book_reviews.size >= 2
-            errors.add(:review_id, "cannot be created since you've already reviewed this book!")
-        end
-    end
-end
+    validates :title, :description, :rating, :reviews, presence: true 
+    belongs_to :user
+    has_many :comments
+    has_many :users, through: :comments 
+    
+  
+      def self.deleted_user
+          Review.all.each do |reviews|
+              if reviews.user == nil 
+                  reviews.destroy
+              end
+          end
+      end
+  
+  
+      def self.ranked_reviews 
+          reviews = Review.order("reviews DESC")
+          return reviews 
+      end
+  
+  end
