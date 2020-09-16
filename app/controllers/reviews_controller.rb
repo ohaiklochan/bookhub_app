84a8057000a929e777edc66500e6804a3e43cd97
ohaiklochan <59032381+ohaiklochan@users.noreply.github.com>
2020-09-16 18:11:14 -0500

    added search functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,12 @@ class ReviewsController < ApplicationController
   before_action :current_review, only: [:show, :edit, :update, :destroy] 
 
   def index
-      @reviews = Review.all 
+    if params[:title]
+        @reviews = Review.find_by_title(params[:title])
+    else
+        @reviews = Review.order(sort_column + " ")
+    end
+    #   @review = Review.find_by_id(params[:id])
   end
 
   def new
@@ -31,26 +36,34 @@ class ReviewsController < ApplicationController
       end
   end
 
+    def update
+        if @review.update(review_params)
+            redirect_to review_path(@review)
+        else 
+            redirect_to edit_review_path 
+        end
+    end
 
-  def update
-      if @review.update(review_params)
-          redirect_to review_path(@review)
-      else 
-          redirect_to edit_review_path 
-      end
-  end
+    def destroy
+        if current_user 
+            @review.destroy 
+            redirect_to reviews_path
+        end
+    end
 
-  def destroy
-      if current_user 
-          @review.destroy 
-          redirect_to reviews_path
-      end
-  end
+    # def search
+    #     @reviews = Review.search(params[:search])
+    #     redirect_to search_review_path
+    # end
 
   private
 
+  def sort_column
+    Review.order(:title).include?(params[:sort]) ? params[:sort] : "title"
+  end
+
   def review_params
-      params.require(:review).permit(:title, :description, :rating, :reviews)
+      params.require(:review).permit(:title, :description, :rating, :reviews, :search)
   end
 
 
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,20 +3,36 @@ class Review < ApplicationRecord
     belongs_to :user
     has_many :comments
     has_many :users, through: :comments 
+    scope :alpha, -> { order(:title) }
+    scope :find_by_title, -> (title) { where("title LIKE ?", title) }
+
     
   
-      def self.deleted_user
-          Review.all.each do |reviews|
-              if reviews.user == nil 
-                  reviews.destroy
-              end
-          end
-      end
+    def self.deleted_user
+        Review.all.each do |reviews|
+            if reviews.user == nil 
+                reviews.destroy
+            end
+        end
+    end
   
   
-      def self.ranked_reviews 
-          reviews = Review.order("reviews DESC")
-          return reviews 
-      end
+    def self.ranked_reviews 
+        reviews = Review.order("reviews DESC")
+        return reviews 
+    end
+
+    def self.search(search)
+        if search
+            review = Review.find_by(title: search)
+            if review
+                self.where(review_id: review)
+            else
+                Review.all
+            end
+        else
+            Review.all
+        end
+    end
   
   end
